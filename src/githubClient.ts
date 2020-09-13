b2b9d9d6d617402b296b5bb46451cc67875cbb25
ia17011 <cfoyvlird@gmail.com>
2020-09-13 21:46:13 +0900

    refactor: 💡 some

--- src/githubClient.ts
+++ src/githubClient.ts
@@ -1,12 +1,14 @@
 import {Octokit} from '@octokit/rest'
-import {RepositoryItem} from './@types/type'
+import {Packages, RepositoryItem} from './@types/type'
+import {notNull} from './utils/utils'
+import ora = require('ora')
 
 const octokit = new Octokit()
 
 /**
  * get single repository that hit with query(repositoryName)
  */
-export async function searchRepository(repositoryName: string) {
+async function searchRepository(repositoryName: string) {
   return octokit.search
     .repos({
       q: `${repositoryName} in:name`,
@@ -16,4 +18,24 @@ export async function searchRepository(repositoryName: string) {
       return res.data.items[0] as RepositoryItem
     })
     .catch(() => null)
+}
+
+/**
+ * get repository Items
+ */
+export async function getRepositoryItems(packages: Packages) {
+  const spinner = ora('Searching package repository on GitHub…').start()
+
+  let repositoryItems: (RepositoryItem | null)[] = []
+  for (const pkg of packages) {
+    const repo = await searchRepository(pkg)
+    repositoryItems.push(repo)
+  }
+  const nonNullablesRepositoryItems: RepositoryItem[] = repositoryItems.filter(
+    notNull,
+  )
+
+  spinner.stop()
+
+  return nonNullablesRepositoryItems
 }
